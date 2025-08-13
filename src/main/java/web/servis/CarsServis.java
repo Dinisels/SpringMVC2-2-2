c93ef029fa3fefac653c99c0454f56276afdb491
Dinicels <asitack15@gmail.com>
2025-08-13 13:23:07 +0300

    v1

---
+++ src/main/java/web/servis/CarsServis.java
@@ -0,0 +1,41 @@
+package web.servis;
+
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarsServis {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    public CarsServis() {
+        cars.add(new Car("Toyota", "Camry", 2020));
+        cars.add(new Car("Honda", "Accord", 2019));
+        cars.add(new Car("BMW", "X5", 2021));
+        cars.add(new Car("Audi", "A4", 2018));
+        cars.add(new Car("Mercedes", "C-Class", 2022));
+        cars.add(new Car("Mercedes", "C-Class", 2022));
+        cars.add(new Car("Tesla", "Model 3", 2023));
+        cars.add(new Car("Ford", "Mustang", 2020));
+        cars.add(new Car("Chevrolet", "Camaro", 2021));
+        cars.add(new Car("Volkswagen", "Golf", 2019));
+        cars.add(new Car("Hyundai", "Tucson", 2022));
+    }
+
+    public List<Car> getCars(int count) {
+        if (count <= 0 || count > cars.size()) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
+
+    public  List<Car> getCars() {
+        return cars;
+    }
+
+
+}
